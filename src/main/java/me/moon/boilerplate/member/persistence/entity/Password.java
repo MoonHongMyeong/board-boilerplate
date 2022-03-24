0612fab4e45b6105cfce3c6b82687c515251749e
MoonHongMyeong <moonhongmyeong@gmail.com>
2022-03-24 23:49:32 +0900

    Refactor : Add missing password validate annotation [#3]

--- src/main/java/me/moon/boilerplate/member/persistence/entity/Password.java
+++ src/main/java/me/moon/boilerplate/member/persistence/entity/Password.java
@@ -9,15 +9,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Password {
 
+    @NotEmpty
     @Column(name = "password", nullable = false)
     private String value;
 
+    @NotEmpty
     @Column(name = "password_failed_count", nullable = false)
     private int failedCount;
 
