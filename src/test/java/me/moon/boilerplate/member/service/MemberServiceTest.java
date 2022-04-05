df0755c9e267769881b18d9b362c6c25cc7c2466
MoonHongMyeong <moonhongmyeong@gmail.com>
2022-04-05 14:21:46 +0900

    Add : add Member sign up service unit test [#3]

---
+++ src/test/java/me/moon/boilerplate/member/service/MemberServiceTest.java
@@ -0,0 +1,121 @@
+package me.moon.boilerplate.member.service;
+
+import me.moon.boilerplate.config.TestProfile;
+import me.moon.boilerplate.member.dto.MemberResponse;
+import me.moon.boilerplate.member.dto.MemberSignupRequest;
+import me.moon.boilerplate.member.exception.EmailDuplicatedException;
+import me.moon.boilerplate.member.persistence.entity.Address;
+import me.moon.boilerplate.member.persistence.entity.Email;
+import me.moon.boilerplate.member.persistence.entity.Member;
+import me.moon.boilerplate.member.persistence.entity.Password;
+import me.moon.boilerplate.member.persistence.repository.MemberRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles(TestProfile.TEST)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class MemberServiceTest {
+
+    private Member member;
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @BeforeAll
+    public void setup(){
+        member = Member.builder()
+                .address(Address
+                        .builder()
+                        .address1("TestAddress1")
+                        .address2("TestAddress2")
+                        .zipcode("TestZipcode")
+                        .build())
+                .email(Email
+                        .builder()
+                        .value("testEmail@test.com")
+                        .build())
+                .password(Password
+                        .builder()
+                        .value("TestPassword")
+                        .build())
+                .name("TestUser")
+                .phone("000-0000-0000")
+                .build();
+    }
+
+    @DisplayName("회원가입 성공")
+    @Test
+    public void successfullySignUp(){
+        //given
+        final Email email = member.getEmail();
+        final Address address = member.getAddress();
+        final Password password = member.getPassword();
+        final String name = member.getName();
+        final String phone = member.getPhone();
+
+        final MemberSignupRequest dto = MemberSignupRequest
+                .builder()
+                .address(address)
+                .email(email)
+                .password(password)
+                .name(name)
+                .phone(phone)
+                .build();
+
+        given(memberRepository.existsByEmail(any())).willReturn(false);
+        given(memberRepository.save(any())).willReturn(member);
+
+        //when
+        final MemberResponse signUpMember = memberService.signup(dto);
+
+        //then
+        assertNotNull(signUpMember);
+        assertEquals(signUpMember.getAddress(), member.getAddress());
+        assertEquals(signUpMember.getEmail(), member.getEmail());
+        assertEquals(signUpMember.getName(), member.getName());
+        assertEquals(signUpMember.getPhone(), member.getPhone());
+    }
+
+    @DisplayName("회원가입 시 이메일 중복")
+    @Test
+    public void duplicatedEmailExceptionWhenSignUp(){
+        //given
+        final Email email = member.getEmail();
+        final Address address = member.getAddress();
+        final Password password = member.getPassword();
+        final String name = member.getName();
+        final String phone = member.getPhone();
+
+        final MemberSignupRequest dto = MemberSignupRequest
+                .builder()
+                .address(address)
+                .email(email)
+                .password(password)
+                .name(name)
+                .phone(phone)
+                .build();
+
+        //when
+        when(memberRepository.existsByEmail(any())).thenReturn(true);
+        //then
+        assertThrows(EmailDuplicatedException.class, () -> {
+            memberService.signup(dto);
+        });
+    }
+}
