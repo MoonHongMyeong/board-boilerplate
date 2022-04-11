522e2734f2d5cbb3a59f920ebfa4080b2c94b347
MoonHongMyeong <moonhongmyeong@gmail.com>
2022-04-11 11:14:58 +0900

    Fix : modify password update dto's regex [#3]

--- src/main/java/me/moon/boilerplate/member/dto/MemberPasswordUpdateRequest.java
+++ src/main/java/me/moon/boilerplate/member/dto/MemberPasswordUpdateRequest.java
@@ -13,12 +13,12 @@ public class MemberPasswordUpdateRequest {
 
     @NotBlank
     @Pattern(message = "잘못된 비밀번호 형식입니다. \n비밀번호는 특수문자, 영문대소문자, 숫자를 포함하여 10자 이상입니다.",
-            regexp = "^[a-zA-Z0-9 !@#$%^&*(),./?_]{10,}$")
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\\\S+$).{10,}$")
     private String oldPassword;
 
     @NotBlank
     @Pattern(message = "잘못된 비밀번호 형식입니다. \n비밀번호는 특수문자, 영문대소문자, 숫자를 포함하여 10자 이상입니다.",
-            regexp = "^[a-zA-Z0-9 !@#$%^&*(),./?_]{10,}$")
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\\\S+$).{10,}$")
     private String newPassword;
 
     @Builder
